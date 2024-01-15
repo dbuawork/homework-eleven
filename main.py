0a6adb748a652f93d451fb78307946d4fc637d44
dbuawork <dbuawork@gmail.com>
2024-01-16 01:11:49 +0200

    third-part and finish

--- main.py
+++ main.py
@@ -36,3 +36,25 @@ processed_text, replacements_count = process_text(text, forbidden_word)
 print("Результат:")
 print(processed_text)
 print(f"\nСтатистика: {replacements_count} заміни(-в) слова '{forbidden_word}'.")
+
+
+
+##Запис слов, що складаються не менше ніж з семи літер, у новий файл
+
+def filter_words(input_file_path, output_file_path, min_length=7):
+    try:
+        with open(input_file_path, 'r', encoding='utf-8') as input_file:
+            content = input_file.read()
+
+            words = content.split()
+            filtered_words = [word for word in words if len(word) >= min_length]
+
+        with open(output_file_path, 'w', encoding='utf-8') as output_file:
+            output_file.write(' '.join(filtered_words))
+
+        print(f"Файл {output_file_path} був створений та заповнений словами з довжиною більше або рівно {min_length} літер.")
+    except Exception as e:
+        print(f"Виникла помилка: {e}")
+
+# Виклик функції для вашого вхідного файлу та нового файлу
+filter_words('text.txt', 'text2.txt', min_length=7)
